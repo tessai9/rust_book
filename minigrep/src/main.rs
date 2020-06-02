bf81df17b3cd5dce01563408ebf50035fae0ff94
tesao <shimeji9@gmail.com>
2020-06-02 19:46:34 +0900

    コードをリファクタリング

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     // 実行時引数の取得
@@ -15,30 +16,8 @@ fn main() {
     println!("searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    print!("The contents is:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    };
 }
